1d720f5270ac39c334b3cb50e0fed54ee887ffc6
Ovi Ahsan <oviahsan12@gmail.com>
2022-08-16 10:51:57 +0600

    Add project files.

---
+++ ASP.NETCoreIdentityCustom/Models/PaperUseage.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NETCoreIdentityCustom.Models
+{
+    public class PaperUseage : BaseModel
+    {
+        [Key]
+        public int PaperUseageID { get; set; }
+        public int MachineId { get; set; }      
+        public int CurrentUses { get; set; }
+        public Machine Machine{ get; set; }
+    }
+}
